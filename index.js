a37e5d78482fa4b1aa15281eb860cff8d5085acd
Noah <noahhughesph@gmail.com>
2023-07-06 17:24:20 -0400

    done

--- index.js
+++ index.js
@@ -1,23 +1,90 @@
-/* Your Code Here */
+const createEmployeeRecord = (employeeArray) => {
+  return {
+    firstName: employeeArray[0],
+    familyName: employeeArray[1],
+    title: employeeArray[2],
+    payPerHour: employeeArray[3],
+    timeInEvents: [],
+    timeOutEvents: [],
+  };
+};
 
-/*
- We're giving you this function. Take a look at it, you might see some usage
- that's new and different. That's because we're avoiding a well-known, but
- sneaky bug that we'll cover in the next few lessons!
+const createEmployeeRecords = (employeeRecords) => {
+  const newEmployeeRecords = employeeRecords.map((element) =>
+    createEmployeeRecord(element)
+  );
+  return newEmployeeRecords;
+};
 
- As a result, the lessons for this function will pass *and* it will be available
- for you to use if you need it!
- */
+const createTimeInEvent = function (timeStamp) {
+  const newTimeInEvent = {
+    type: 'TimeIn',
+    hour: parseInt(timeStamp.slice(11)),
+    date: timeStamp.slice(0, 10),
+  };
+  this.timeInEvents.push(newTimeInEvent);
+  return this;
+};
+
+const createTimeOutEvent = function (timeStamp) {
+  const newTimeOutEvent = {
+    type: 'TimeOut',
+    hour: parseInt(timeStamp.slice(11)),
+    date: timeStamp.slice(0, 10),
+  };
+  this.timeOutEvents.push(newTimeOutEvent);
+  return this;
+};
+
+const hoursWorkedOnDate = function (dateStamp) {
+  const timeInArray = this.timeInEvents;
+  const timeInDay = timeInArray
+    .filter((element) => element.date === dateStamp)
+    .map((element) => element.hour);
+  const timeInHour = parseInt(timeInDay);
+
+  const timeOutArray = this.timeOutEvents;
+  const timeOutDay = timeOutArray
+    .filter((element) => element.date === dateStamp)
+    .map((element) => element.hour);
+  const timeOutHour = parseInt(timeOutDay);
+
+  return (timeOutHour - timeInHour) / 100;
+};
+
+const wagesEarnedOnDate = function (dateStamp) {
+  return this.payPerHour * hoursWorkedOnDate.call(this, dateStamp);
+};
 
 const allWagesFor = function () {
-    const eligibleDates = this.timeInEvents.map(function (e) {
-        return e.date
-    })
+  const eligibleDates = this.timeInEvents.map(function (e) {
+    return e.date;
+  });
 
-    const payable = eligibleDates.reduce(function (memo, d) {
-        return memo + wagesEarnedOnDate.call(this, d)
-    }.bind(this), 0) // <== Hm, why did we need to add bind() there? We'll discuss soon!
+  const payable = eligibleDates.reduce(
+    function (memo, d) {
+      return memo + wagesEarnedOnDate.call(this, d);
+    }.bind(this),
+    0
+  ); // <== Hm, why did we need to add bind() there? We'll discuss soon!
 
-    return payable
-}
+  return payable;
+};
 
+const findEmployeeByFirstName = function (srcArray, firstName) {
+  const filteredArray = srcArray.find(
+    (e) => e.firstName === firstName
+  );
+  if (filteredArray === {}) {
+    return undefined;
+  }
+  return filteredArray;
+};
+
+const calculatePayroll = function (employeeRecords) {
+  const payRoll = employeeRecords.reduce(
+    (total, element) => (total += allWagesFor.call(element)),
+    0
+  );
+  return payRoll;
+};
